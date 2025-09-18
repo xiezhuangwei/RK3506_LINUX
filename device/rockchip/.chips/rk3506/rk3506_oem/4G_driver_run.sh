c51dcf70029023df8dc29ef26338bc8a2eb68b5c
谢壮伟 <xiezw@tuya.com>
2025-09-18 22:54:42 +0800

    [4G]：支持USB串口驱动，自动执行初始化脚本

---
+++ device/rockchip/.chips/rk3506/rk3506_oem/4G_driver_run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+
+echo "4G driver run!!!"
+echo 0 > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio0/direction 
+echo 0 > /sys/class/gpio/gpio0/value 
+
+sleep 1
+insmod /oem/usb/serial/usbserial.ko
+sleep 1
+insmod /oem/usb/serial/usb_wwan.ko
+sleep 1
+insmod /oem/usb/serial/option.ko
+sleep 1
+
+# 绑定 ML307B 设备
+echo "2c7c 0125" > /sys/bus/usb-serial/drivers/option/new_id 2>/dev/null || \
+echo "2c7c 0125" > /sys/bus/usb-serial/drivers/usbserial/new_id
+
+
--- device/rockchip/.chips/rk3506/rk3506_oem/RkLunch.sh
+++ device/rockchip/.chips/rk3506/rk3506_oem/RkLunch.sh
@@ -1,1 +1,3 @@
 echo "The user's boot script is executed here"
+
+/oem/4G_driver_run.sh&
