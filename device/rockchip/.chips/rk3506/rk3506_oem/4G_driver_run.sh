3fccd12636908fcd53256e5ce29e40d47c40162b
ÄãµÄÃû <694332875@qq.com>
2025-09-19 22:28:36 +0800

    [drv]：支持4G qim拨号模式

--- device/rockchip/.chips/rk3506/rk3506_oem/4G_driver_run.sh
+++ device/rockchip/.chips/rk3506/rk3506_oem/4G_driver_run.sh
@@ -10,12 +10,15 @@ echo 0 > /sys/class/gpio/gpio0/value
 sleep 1
 insmod /oem/usb/serial/usbserial.ko
 sleep 1
-insmod /oem/usb/serial/usb_wwan.ko
+#insmod /oem/usb/serial/usb_wwan.ko
+#insmod /oem/usb/serial/cdc-wdm.ko
 sleep 1
-insmod /oem/usb/serial/option.ko
+#insmod /oem/usb/serial/option.ko
+#insmod /oem/usb/serial/qmi_wwan.ko
 sleep 1
 
-# 绑定 ML307B 设备
-echo "37d4 a002" > /sys/bus/usb-serial/drivers/option1/new_id
+# ttyUSB AT 
+#echo "37d4 a002" > /sys/bus/usb-serial/drivers/option1/new_id
 
-
+#cdc-wdm0 wlan
+echo "37d4 a002" > /sys/bus/usb/drivers/qmi_wwan/new_id
