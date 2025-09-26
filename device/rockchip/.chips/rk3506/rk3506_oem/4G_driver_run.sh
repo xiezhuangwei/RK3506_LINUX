56b1d7fd1f271a24e4a9dccb0f06c47465624d2d
谢壮伟 <xiezw@tuya.com>
2025-09-26 21:43:19 +0800

    [4G]：增加AT配置拨号模式

--- device/rockchip/.chips/rk3506/rk3506_oem/4G_driver_run.sh
+++ device/rockchip/.chips/rk3506/rk3506_oem/4G_driver_run.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
+function wwan_mode() {
+	#insmod /oem/usb/serial/cdc-wdm.ko
+	sleep 1
+	#insmod /oem/usb/serial/qmi_wwan.ko
+	sleep 1
+	#cdc-wdm0 wlan
+	#echo "37d4 a002" > /sys/bus/usb/drivers/qmi_wwan/new_id
+}
 
+function rndis_mode() {
+	#insmod /oem/usb/serial/usb_wwan.ko
+	sleep 1
+	#insmod to/oem/usb/serial/option.ko
+	sleep 1
+	# ttyUSB AT 
+	echo "37d4 a002" > /sys/bus/usb-serial/drivers/option1/new_id
+	
+	#cat /dev/ttyUSB3 &
+	echo "AT+MDIALUPCFG=mode,0" > /dev/ttyUSB3
+	echo "AT+MDIALUPCFG=workmode,1" > /dev/ttyUSB3
+	echo "AT+MDIALUP=1,1" > /dev/ttyUSB3
+	
+}
 
 echo "4G driver run!!!"
 echo 0 > /sys/class/gpio/export
@@ -10,15 +32,6 @@ echo 0 > /sys/class/gpio/gpio0/value
 sleep 1
 insmod /oem/usb/serial/usbserial.ko
 sleep 1
-#insmod /oem/usb/serial/usb_wwan.ko
-#insmod /oem/usb/serial/cdc-wdm.ko
-sleep 1
-#insmod /oem/usb/serial/option.ko
-#insmod /oem/usb/serial/qmi_wwan.ko
-sleep 1
+rndis_mode
 
-# ttyUSB AT 
-#echo "37d4 a002" > /sys/bus/usb-serial/drivers/option1/new_id
 
-#cdc-wdm0 wlan
-echo "37d4 a002" > /sys/bus/usb/drivers/qmi_wwan/new_id
